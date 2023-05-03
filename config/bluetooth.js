059c5b94ef14e7e1b0fc2a64725506840b330598
Victor Oliveira <103453296+Victor23HD@users.noreply.github.com>
2023-05-03 10:51:31 -0300

    Add files via upload

--- config/bluetooth.js
+++ config/bluetooth.js
@@ -1,22 +1,33 @@
-const ConnectDevice = () => 
-{
-    navigator.bluetooth.requestDevice(options)
-    .then((device) => 
-    {
-        console.log("Conectando no dispositivo...");
-        return device.gatt.connect();
-
-    })
-    .catch((err) => console.log(err));
-}
-
+let bluetoothDevice = null;
+let IsConnect = false;
 
 let options = {
-    filters: [
-        { name: "ExampleName" },
-        { namePrefix: "PAX" }
-      ],
-      optionalServices: ["battery_service"],
+  filters: [{ name: "ExampleName" }, { namePrefix: "PAX" }],
+  optionalServices: ["battery_service"],
 };
+
+
+async function ConnectDevice() {
+  await navigator.bluetooth
+    .requestDevice(options)
+    .then(async (device) => {
+      console.log("> Buscando dispositivo bluetooth...");
+      bluetoothDevice = device;
+      await Connect();
+    })
+    .catch((err) => console.log(err));
+
+    return IsConnect != false ? "Conectado" : "Desconectado";
+
+}
+
+async function Connect() {
+  console.log("> conectando ao dispositivo bluetooth...");
+
+  await bluetoothDevice.gatt.connect().then((server) => {
+    console.log("> Dispositivo bluetooth conectado.");
+    return IsConnect = true
+  });
+}
 
 export default ConnectDevice;
--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,36 @@
 import { useState } from "react";
-import {
-  ArrowUpIcon,
-  ArrowUpTrayIcon,
-  MinusCircleIcon,
-  PlusCircleIcon,
-} from "@heroicons/react/24/solid";
+import { ArrowUpTrayIcon} from "@heroicons/react/24/solid";
 
-import ConnectDevice from "../config/bluetooth.js"
+import ConnectDevice from "../config/bluetooth.js";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState("");
 
   return (
     <>
-      <img src="/public/Images/BookBanner.jpg" />
       <div className="flex justify-center items-center h-screen bg-slate-800">
         <div className="text-center bg-white rounded-md p-5 shadow-lg text-sm">
-          <h1 className="font-semibold text-lg">OliveiraBookstore</h1>
-          <h2 className="font-medium text-gray-600">
-            O melhor lugar para suas compras!
+          <h1 className="font-semibold text-lg">Pax-Solutions</h1>
+          <h2 className="font-medium text-gray-600">  
+            Comunicação bluetooth com POS
           </h2>
           <div className="card">
-            <div className="flex flex-row justify-center items-center m-2 font-medium">
-             
-              <button
-                className="px-6 py-2 bg-blue-600 rounded-lg text-white hover:bg-blue-800 transition duration-100"
-                onClick={() =>
-                  setCount((count) => (count > 0 ? count - 1 : (count = 0)))
-                }
-              >
-                <MinusCircleIcon className="w-5 scale-110" />
-              </button>
-
-              <label className="mx-4"> {count} </label>
-              
-              <button
-                className="px-6 py-2 bg-blue-600 rounded-lg text-white hover:bg-blue-800 transition duration-1000"
-                onClick={() => setCount(count + 1)}
-              >
-                <PlusCircleIcon className="w-5 scale-110" />
-              </button>
-
-            </div>
-            <div>
-             
+            <div className="flex justify-center items-center m-2 font-medium">
               <button
                 className="bg-blue-600 rounded-lg px-6 py-2 m-1"
-                onClick={(ConnectDevice())}
-              >
-                <ArrowUpTrayIcon className="w-5" />
+                onClick={async() => {
+                  setStatus(await ConnectDevice());
+                }}>
+                <ArrowUpTrayIcon className="w-6 text-white font-bold" />
               </button>
+              <label>{status}</label>
             </div>
+
+            
+
           </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
+          <p className="text-gray-500 font-medium">
+          PAX Technology © 2023
           </p>
         </div>
       </div>
